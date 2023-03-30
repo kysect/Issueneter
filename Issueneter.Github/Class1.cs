145dbcb720e6f32db2b8ba523173ff56105faa43
hrrrrustic <hrrrrustic@gmail.com>
2023-03-30 17:58:18 +0300

    Add github project

---
+++ Issueneter.Github/Class1.cs
@@ -0,0 +1,35 @@
+﻿using Octokit.GraphQL;
+using static Octokit.GraphQL.Variable;
+
+namespace Issueneter.Github;
+
+public class Class1
+{
+    public static async Task Test()
+    {
+        var productInformation = new ProductHeaderValue("YOUR_PRODUCT_NAME", "YOUR_PRODUCT_VERSION");
+        var connection = new Connection(productInformation, "YOUR_OAUTH_TOKEN");
+
+            .RepositoryOwner(Var("owner"))
+            .Repository(Var("name"))
+            .Issues().Nodes
+            .Select(repo => new
+            {
+                repo.Id,
+                repo.Name,
+                repo.Owner.Login,
+                repo.IsFork,
+                repo.IsPrivate,
+            }).Compile();
+
+        var vars = new Dictionary<string, object>
+        {
+            { "owner", "octokit" },
+            { "name", "octokit.graphql.net" },
+        };
+
+        var result =  await connection.Run(query, vars);
+
+        Console.WriteLine(result.Login + " & " + result.Name + " Rocks!");
+    }
+}
--- Migrations/20230329222150_initial_migration.sql
+++ Migrations/20230329222150_initial_migration.sql
@@ -1,13 +1,23 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE SCHEMA IF NOT EXISTS issueneter;
+
 CREATE TABLE IF NOT EXISTS issueneter.scans (
     id SERIAL NOT NULL
-        CONSTRAINT scan_pkey PRIMARY KEY;
+        CONSTRAINT scan_pkey PRIMARY KEY,
+    scan_type SMALLINT NOT NULL,
+    accOrOrg varchar NOT NULL,
+    repo varchar NULL,
+    created TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    filters JSON NOT NULL
 );
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+
+DROP TABLE issueneter.scans;
+DROP SCHEMA issueneter;
+
 -- +goose StatementEnd
