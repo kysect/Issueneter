1ccf05dc7fb6be60a80f49dfe6848cc995659c96
bibletoon <alex8.ivanov@gmail.com>
2023-05-21 23:55:49 +0300

    Fix: remove hand-written scan sources

--- Issueneter.Host/Routes/ScanSourceRoutes.cs
+++ Issueneter.Host/Routes/ScanSourceRoutes.cs
@@ -1,18 +1,9 @@
-﻿using Issueneter.Annotation;
-using Issueneter.Host.Responses;
-using Issueneter.Mappings;
+﻿using Issueneter.Mappings;
 
 namespace Issueneter.Host.Routes;
 
 public static class ScanSourceRoutes
 {
-    // TODO: Засурсгенить
-    private static readonly Dictionary<string, AvailableSourceResponse[]> Availables = new()
-    {
-        ["Issue"] = new[] {new AvailableSourceResponse("Issue author", "CreatedBy"), new AvailableSourceResponse("Is open", "State")},
-        ["PullRequest"] = new[] {new AvailableSourceResponse("Pull request author", "CreatedBy"), new AvailableSourceResponse("State", "State")}
-    };
-
     public static WebApplication MapScanSourceRoutes(this WebApplication app)
     {
         app.MapGet("/available_sources", () => ModelsInfo.AvailableScanSources).WithOpenApi();
