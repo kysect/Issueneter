676bfd0bd015e3386ee16fde5687d6cf92db3443
bibletoon <alex8.ivanov@gmail.com>
2023-05-22 00:09:30 +0300

    Fix: change scan source type in db

--- Migrations/20230329222150_initial_migration.sql
+++ Migrations/20230329222150_initial_migration.sql
@@ -5,9 +5,9 @@ CREATE SCHEMA IF NOT EXISTS issueneter;
 CREATE TABLE IF NOT EXISTS issueneter.scans (
     id SERIAL NOT NULL
         CONSTRAINT scan_pkey PRIMARY KEY,
-    scan_type SMALLINT NOT NULL,
-    owner varchar NOT NULL,
-    repo varchar NULL,
+    scan_type text NOT NULL,
+    owner text NOT NULL,
+    repo text NULL,
     chat_id bigint NOT NULL,
     created TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     filters JSON NOT NULL
